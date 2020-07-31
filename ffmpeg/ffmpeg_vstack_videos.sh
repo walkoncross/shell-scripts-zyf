58e3bed76bc64b1e4720ae3ff1eeebdbe26534d3
zhaoyafei <zhaoyafei@baidu.com>
2020-07-31 17:12:48 +0800

    update ffmpeg/*.sh

--- ffmpeg/ffmpeg_vstack_videos.sh
+++ ffmpeg/ffmpeg_vstack_videos.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-## Vertically stack two videos with the same size, using ffmpeg
 #! Author: zhaoyafei0210@gmail.com
+
+## Vertically stack two videos with the same size, using ffmpeg
+# Stream mapping (example):
+#   Stream #0:0 (h264) -> hstack:input0 (graph 0)
+#   Stream #1:0 (h264) -> hstack:input1 (graph 0)
+#   vstack (graph 0) -> Stream #0:0 (libx264)
+#   Stream #0:1 -> #0:1 (aac (native) -> aac (native))
 
 out_video='output.mp4'
 if [[ $# -gt 2 ]]; then
