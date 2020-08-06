2d11eff336be46421dedccbf87dc9593a20c0637
zhaoyafei <zhaoyafei@baidu.com>
2020-08-06 17:36:36 +0800

    add ffmpeg_python_tools/align_videos.py

--- ffmpeg_python/ffmpeg_python_tools/__init__.py
+++ ffmpeg_python/ffmpeg_python_tools/__init__.py
@@ -2,5 +2,6 @@ from .stream_info import *
 from .extract_audio import *
 from .stack_videos import *
 from .trim_audio import *
-from .align_offsets import *
 from .video_groups import *
+from .align_offsets import *
+from .align_videos import *
