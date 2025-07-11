2a7aefac8fc655e413ece115eefa5c6f0de6c22f
zhaoyafei <zhaoyafei0210@gmail.com>
2025-07-11 10:11:18 +0800

    add vscode/export_extension_list.sh

---
+++ vscode/export_extension_list.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# ## author: zhaoyafei0210@gmail.com
+# This script exports the list of installed Visual Studio Code extensions to a file.
+# Usage: ./export_extension_list.sh
+code --list-extensions > vscode_extensions_list.txt
